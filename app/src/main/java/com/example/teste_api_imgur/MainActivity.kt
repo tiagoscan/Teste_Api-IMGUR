80c70db96405a900c4bb2786fa955404231d52d2
Tiago <tiagocan@gmail.com>
2024-07-11 23:33:46 -0300

    Criado Binding

--- app/src/main/java/com/example/teste_api_imgur/MainActivity.kt
+++ app/src/main/java/com/example/teste_api_imgur/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.teste_api_imgur
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.teste_api_imgur.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate( layoutInflater )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        setContentView( binding.root )
     }
 }
